8fa215f4e57abe0c1a165648141c74719837e8d8
Edmund Dipple <elmundio1987@gmail.com>
2015-10-20 12:43:36 +0100

    Update dependencies

--- dependencies.sh
+++ dependencies.sh
@@ -1,3 +1,5 @@
+go get golang.org/x/crypto/openpgp
+go get github.com/cryptix/go/logging
 go get github.com/sloonz/go-iconv
 go get github.com/garyburd/redigo/redis
 go get github.com/sloonz/go-qprintable
